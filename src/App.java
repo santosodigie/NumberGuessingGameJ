b28bd41905ba5a5f78cf4b3292b9f2dfcadc9214
Santos Odigie <santosodigie104@gmail.com>
2024-11-11 15:05:10 +0000

    cpu generates a number between 1 and 10

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Welcome to my Number Guessing Game!");
+        Random rand = new Random();
+        int number = rand.nextInt(1, 11);
+        boolean exit = false;
+        System.out.println(number);
+
+        // while(!exit){
+        //     System.out.println("Please guess a number");
+        // }
     }
 }
