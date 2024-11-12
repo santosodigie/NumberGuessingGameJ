00839ff680a66fc7ecd78de06b09288ccae82f20
Santos Odigie <santosodigie104@gmail.com>
2024-11-12 22:06:00 +0000

    completed base logic for number guessing game

--- src/App.java
+++ src/App.java
@@ -2,14 +2,23 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, Welcome to my Number Guessing Game!");
         Random rand = new Random();
         int number = rand.nextInt(1, 11);
         boolean exit = false;
-        System.out.println(number);
 
-        // while(!exit){
-        //     System.out.println("Please guess a number");
-        // }
+        while(!exit){
+            System.out.println("Please guess a number");
+            String guess = scanner.nextLine();
+
+            if(Integer.parseInt(guess) == number){
+                System.out.println("Congrats you have won ");
+                exit = true;
+            }
+            else{
+                System.out.println("Please guess again");
+            }
+        }
     }
 }
