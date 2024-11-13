0c57710e80a81a30da5e213c7aced67018468cd4
Santos Odigie <santosodigie104@gmail.com>
2024-11-13 22:20:43 +0000

    Added counter and hints

--- src/App.java
+++ src/App.java
@@ -7,17 +7,27 @@ public class App {
         Random rand = new Random();
         int number = rand.nextInt(1, 11);
         boolean exit = false;
+        int count = 0;
 
         while(!exit){
             System.out.println("Please guess a number");
             String guess = scanner.nextLine();
+            //System.out.println("Count: " + count);
+            count++;
 
             if(Integer.parseInt(guess) == number){
-                System.out.println("Congrats you have won ");
+                System.out.println("Congrats you have won. It took you " + count + " tries");
                 exit = true;
             }
+            else if(Integer.parseInt(guess) > number){
+                System.out.println("Please guess lower");
+            }
+            else if(Integer.parseInt(guess) < number){
+                System.out.println("Please guess higher");
+                System.out.println("____________________________________");
+            }
             else{
-                System.out.println("Please guess again");
+                System.out.println("Please enter a valid number");
             }
         }
     }
